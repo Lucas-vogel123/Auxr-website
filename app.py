3d883c62966f01f95d023730337d0b840782be37
lucas-vogel123 <lucasvogel1050@gmail.com>
2023-05-08 21:36:27 -0700

    back end is working. few bugs left.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
 
@@ -20,12 +21,10 @@ def ambassador_login():
         password = request.form["password"]
         ambassador = db_session().query(Ambassador).where(Ambassador.email == email and Ambassador.password == password)
         if(ambassador == None):
-            print("ambassador does not exist")
+            flash("ambassador does not exist")
         else:
-            print("Ambassador logged in")
-            pass
-            #print a view with an error
-        return redirect(url_for("ambassador_loggedin"))
+            print("Ambassador logged in")  
+            return render_template("home.html")
     else:
         return render_template("Amlogin.html")
 
@@ -35,18 +34,44 @@ def ambassador_loggedin():
 
 @app.route("/apply", methods = ["GET", "POST"])
 def ambassador_apply():
-    return render_template("Amapply.html")
+    if request.method == "POST":
+        verify = False
+        fname = request.form["firstName"]
+        lname = request.form["lastName"]
+        programs = request.form["socialPrograms"]
+        gender = request.form["gender"]
+        school = request.form["school"]
+        email = request.form["email"]
+        sa1 = request.form["SA1"]
+        sa2 = request.form["SA2"]
+        sa3 = request.form["SA3"]
+        password = request.form["password"]
+        confirm_password = request.form["confirmPassword"]
+        if(password == confirm_password):
+            verify = True
+            print("password is good")
+        else:
+            verify = False
+            print("wrong password")
+        if(verify):
+            new_application = Application(fname, lname, gender, email, school, programs, sa1, sa2, sa3)
+            db_session.add(new_application)
+            db_session.commit()
+            print("application made")
+        return render_template("home.html")
+    
+    else:
+        return render_template("Amapply.html")
 
 @app.route("/newsletter/signup", methods = ["GET", "POST"])
 def newsletter_signup():
     if request.method == "POST":
         email = request.form["email"]
-        ambassador = db_session().query(Ambassador).where(Ambassador.email == email)
-        is_ambassador = True
-        if(ambassador == None):
-            is_ambassador = False
-        new_newsletter = Newsletter(is_ambassador, email)
-        return redirect(url_for("home"))
+        new_newsletter = Newsletter(email)
+        db_session.add(new_newsletter)
+        db_session.commit()
+        print("newsletter made")
+        return render_template("home.html")
     else:
         return render_template("newsletterSignUp.html")
     
